8350dc26847b89cebc065c74464ff0c0e55e6b53
macstudent <ankita.jain12342@gmail.com>
2019-09-19 19:08:57 -0400

    arithmatic class and student class

---
+++ src/main/java/com/lambton/Student.java
@@ -0,0 +1,12 @@
+package com.lambton;
+
+public class Student
+{
+    int Student_id;
+    String FirstName, LastName;
+    int marks[ ];
+    int total;
+    double percentage;
+    String result;
+
+}
